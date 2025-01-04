4c3647c56a72b4b361eafe88ff394dc8bc65f02f
Justin Waite <justindwaite@gmail.com>
2025-01-03 19:38:01 -0700

    inital commit

---
+++ lua/custom/plugins/lsp-signature.lua
@@ -0,0 +1,11 @@
+return {
+  -- Display function signature when typing
+  {
+    'ray-x/lsp_signature.nvim',
+    event = 'VeryLazy',
+    opts = {},
+    config = function(_, opts)
+      require('lsp_signature').setup(opts)
+    end,
+  },
+}
